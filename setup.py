f547177671ebb599d6de160c91bae1591739fafc
Thomas Roy <37151943+tlroy@users.noreply.github.com>
2024-08-27 11:56:45 -0700

    Update setup.py
    update version number in install

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="EchemFEM",
-      version="0.0.1",
+      version="0.0.2",
       description="FEM for electrochemical transport",
       packages=["echemfem"],
       install_requires=["pandas"],
